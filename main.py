6027cd3e605162c17e28cdd74e4ece76d61f1573
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-10-11 21:59:57 +0200

    Prace, under construction

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import cgi
 import webapp2
+import urllib
 from google.appengine.api import users
+from google.appengine.ext import ndb
+
+GLOWNA = """\
+	<form action="/com?%s" method="post">
+		<div><textarea name="tresc" rows="4" cols="60"></textarea></div>
+		<div><input type="submit" value="Postuj"></div>
+	</form>
+	<hr>
+	<form>Obiekt komentu:
+		<input value="%s"
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
