451ab97a65f6078f9c57209c68add8be69bb5784
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-10-11 19:54:35 +0200

    Dodano wymuszenie zalogowania się w Google

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import webapp2
+from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
+		
+		user = users.get_current_user()
+		if user:
+			self.response.headers['Content-Type'] = 'text/plain'
+			self.response.write('Hello, ' + user.nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 		self.response.write(u'Hello worldowy świecie!')
 
 
