3b8bfaec5b794af17e34e90e838288243b9c385f
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-10-11 23:59:31 +0200

    Dalsze prace

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 GLOWNA = """\
-	<form action="/com?%s" method="post">
-		<div><textarea name="tresc" rows="4" cols="60"></textarea></div>
-		<div><input type="submit" value="Postuj"></div>
-	</form>
-	<hr>
-	<form>Obiekt komentu:
-		<input value="%s"
+		<form action="/com?%s" method="post">
+			<div><textarea name="tresc" rows="4" cols="60"></textarea></div>
+			<div><input value="%s" name="adressub"><br>
+				<input type="radi </div>
+			<div><input type="submit" value="Postuj"></div>
+		</form>
+		<hr><br>
+		<form>Adres komentowanej strony:
+			<input value="%s" name="adres"><br>Wyświetlanie:
+			<input type="radio" name="jakshow" value="dokladnie">Dokładnie<br>
+			<input type="radio" name="jakshow" value="narrowed">Uwzględniając mniej precyzyjne komentarze<br>
+			<input type="checkbox" name="jakshowstend" value="widerly">Wszystko zaczynające się od<br>
+			<input type="checkbox" name="jakshowstend" value="widerlysta">Wszystko kończące się na<br>
+			<input type="submit" value="switch">
+		</form>
+		<a href="%s">%s</a>
+	</body>
+</html>
+"""
+
+
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
